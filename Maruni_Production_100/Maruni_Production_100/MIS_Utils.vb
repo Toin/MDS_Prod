cd4334f5e91a76d855f8d7e29bd6df4b4fbb4905
toin <toin.zhang@gmail.com>
2011-03-18 07:10:12 +0700

    v1.0.0.14 MDS Production

---
+++ Maruni_Production_100/Maruni_Production_100/MIS_Utils.vb
@@ -0,0 +1,39 @@
+﻿Option Strict Off
+Option Explicit On
+
+Public Class MIS_Utils
+    Public Function fctFormatDate(ByVal pdate As Date, ByVal oCompany As SAPbobsCOM.Company, Optional ByVal sngFormat As Integer = 5) As String
+        Dim strSeparator As String
+        Dim oGetCompanyService As SAPbobsCOM.CompanyService = Nothing
+        Dim oAdminInfo As SAPbobsCOM.AdminInfo = Nothing
+
+        fctFormatDate = ""
+
+        oGetCompanyService = oCompany.GetCompanyService
+        oAdminInfo = oGetCompanyService.GetAdminInfo
+
+        sngFormat = oAdminInfo.DateTemplate
+        strSeparator = oAdminInfo.DateSeparator
+
+        Select Case sngFormat
+            Case 0
+                fctFormatDate = Format(pdate, "dd") + strSeparator + Format(pdate, "MM") + strSeparator + Format(pdate, "yy")
+            Case 1
+                fctFormatDate = Format(pdate, "dd") + strSeparator + Format(pdate, "MM") + strSeparator + "20" + Format(pdate, "yy")
+            Case 2
+                fctFormatDate = Format(pdate, "MM") + strSeparator + Format(pdate, "dd") + strSeparator + Format(pdate, "yy")
+            Case 3
+                fctFormatDate = Format(pdate, "MM") + strSeparator + Format(pdate, "dd") + strSeparator + "20" + Format(pdate, "yy")
+            Case 4
+                fctFormatDate = "20" + Format(pdate, "yy") + strSeparator + Format(pdate, "MM") + strSeparator + Format(pdate, "dd")
+            Case 5
+                fctFormatDate = Format(pdate, "dd") + strSeparator + Format(pdate, "MMMM") + strSeparator + Format(pdate, "yyyy")
+            Case 6
+                fctFormatDate = Format(pdate, "yy") + strSeparator + Format(pdate, "MM") + strSeparator + Format(pdate, "dd")
+        End Select
+
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(oGetCompanyService)
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(oAdminInfo)
+    End Function
+
+End Class
---
+++ Maruni_Production_100/Maruni_Production_100/Main.vb
@@ -0,0 +1,29 @@
+﻿
+Option Explicit On
+Option Strict Off
+
+Module Main
+
+    Public Sub Main()
+
+        Dim SBOConn As SBOConnection
+        Dim ClsName As String
+        ClsName = "Main"
+
+        Try
+
+            SBOConn = New SBOConnection
+            'MsgBox("Connected! hi!")
+
+            Dim SOToMFG As SOToMFG
+            SOToMFG = New SOToMFG
+
+            System.Windows.Forms.Application.Run()
+
+        Catch ex As Exception
+            MsgBox(ClsName & " Addon failed! SBO Not Running!")
+        End Try
+
+    End Sub
+
+End Module
